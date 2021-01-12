b268faaa8ca0c8f40ec3dbe7971d9536a7c9c870
Jaxon D <duncan5irjax@gmail.com>
2021-01-11 19:59:39 -0600

    Implemented the character limit and first version of the type choices

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,11 @@ var specialChars = " !#$%&()*+,-./:;<=>?@[]^_`{|}~".split(''); //I have read onl
 // Write password to the #password input
 function writePassword() {
   var passwordLength = prompt("How long?")
+  while (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength < 8) {var passProb = "short"}
+    else if (passwordLength > 128) {var passProb = "long"}
+    passwordLength = prompt("Too " + passProb + " must be between 8 and 128")
+  }
   var passwordLower = confirm("Lowercase?");
   var passwordUpper = confirm("Uppercase?");
   var passwordNumbers = confirm("Numbers?");
@@ -27,13 +32,13 @@ var password = '';
     console.log(choiceType)
     if (passwordLower === true && choiceType === 0) {
       password += passwordMaking(lowerCase);
-    } else if (passwordUpper === true && choiceType === 1) {
+    } else if ((passwordUpper === true && choiceType === 1) || (passwordLower === false && choiceType === 0)) {
       password += passwordMaking(upperCase);
-    } else if (passwordNumbers === true && choiceType === 2) {
+    } else if ((passwordNumbers === true && choiceType === 2) || ((passwordLower === false && passwordUpper === false) && choiceType === 0) || ((passwordLower === false && passwordUpper === true) && choiceType === 1)) {
       password += passwordMaking(numbers);
-    } else if (passwordSpecial === true && choiceType === 3) {
+    } else if ((passwordSpecial === true && choiceType === 3) || (((passwordLower === false && passwordUpper === false) && passwordNumbers === false) && choiceType === 0) || (((passwordLower === false && passwordUpper === false) && passwordNumbers === true) && choiceType === 1) || (((passwordLower === true ^ passwordUpper === true) && passwordNumbers === false) && choiceType === 1) || (((passwordLower === true ^ passwordUpper === true) && passwordNumbers === true) && choiceType === 2)) {
       password += passwordMaking(specialChars);
-    }
+    } //Yes that was incredibly messy, any suggestions on how to do that better would be appreciated
     console.log(password)
   }
   return password;
